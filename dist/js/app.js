f604cb68da0bb39c061fbeb98f5435460cb42df3
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-04-24 00:29:47 +0530

    added input form styling

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,5 @@
 /******/ (function() { // webpackBootstrap
 var __webpack_exports__ = {};
-var logMessage = function logMessage(msg) {
-  return console.log(msg);
-};
 
-logMessage("some message");
 /******/ })()
 ;
-//# sourceMappingURL=app.js.map
